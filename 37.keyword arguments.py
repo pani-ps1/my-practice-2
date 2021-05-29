5412b88fe7c3eae3958961d8c6c3bd06ada00e76
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:15:14 -0300

    Add files via upload

---
+++ 37.keyword arguments.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 17:12:02 2020
+
+@author: utob
+"""
+
+def greet_user(first_name, last_name):
+    print(f'hi {first_name} {last_name}!')
+    print('welcom aboard')
+    
+print("start")
+greet_user("john", last_name="smith")
+print("finish")
+
+# nokte in tamrin in bud ke
+# pozoshenal argument bayad ghabl az keyword argument biad
