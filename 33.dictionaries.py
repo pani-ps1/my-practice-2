6977db353c4b9cd600f3f2b8f0c58f965341f19f
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:13:59 -0300

    Add files via upload

---
+++ 33.dictionaries.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 12:14:45 2020
+
+@author: utob
+"""
+
+customer = {
+    "name": "john smith",
+    "age ": 30,
+    "is_verified":True
+    }
+print(customer["name"])
