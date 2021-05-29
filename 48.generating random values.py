1c7ac7acee6c38fa9f482615584bf5902c9102a9
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:20:00 -0300

    Add files via upload

---
+++ 48.generating random values.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 21:21:51 2020
+
+@author: utob
+"""
+
+import random
+
+members = ['john', 'mary', 'bob', 'mosh']
+leader = random.choice(members)
+print(leader)
