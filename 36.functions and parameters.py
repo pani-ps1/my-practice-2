fc778408ab7fae638c0e1642d7e9eb7ef4d912b3
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:14:58 -0300

    Add files via upload

---
+++ 36.functions and parameters.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 12:48:01 2020
+
+@author: utob
+"""
+
+def greet_user(first_name, last_name):
+    print(f'hi {first_name} {last_name}!')
+    print('welcome aboard')
+    
+print("start")
+greet_user("john" , "smith")
+print("finish")
+
