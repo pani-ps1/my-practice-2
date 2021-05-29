3beb64dc52700b9282a313c20bb57c6f74bc0b1d
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:20:19 -0300

    Add files via upload

---
+++ 49.roll the dice 1 khodam.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 17:39:03 2020
+
+@author: utob
+"""
+class Dicea:
+    pass
+
+
+class Diceb:
+    pass
+
+
+class Dice:
+    def rolling (Dicea(dice_members)):
+        return(Dicea)
+
+    def rolling (Diceb(dice_members)):
+        return(Diceb)
+
+        
+import random
+
+dice_members = (1, 2, 3, 4, 5, 6)
+dices = random.choice(dice_members)
+print(Dicea, Diceb)
