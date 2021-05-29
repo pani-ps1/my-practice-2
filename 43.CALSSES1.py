5ab601d8669437b2a112002cdb20768fcadfd55e
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:17:26 -0300

    Add files via upload

---
+++ 43.CALSSES1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 18:27:27 2020
+
+@author: utob
+"""
+# HAME POINT HA BAYAD BOZORG NEVESHTE SHAVAD
+class Point:
+    def move(self):
+        print("move")
+        
+    def draw(self):
+        print("draw")
+        
+
+Point1 = Point()
+Point1.x = 10
+Point1.y = 20
+print(Point1.x)
+Point1.draw()
+
+Point2 = Point()
+Point2.x = 1 
+print(Point2.x)
