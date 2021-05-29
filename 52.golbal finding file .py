0c780a03ba34c7cd116411fa6a0cc9f505b15c65
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:21:56 -0300

    Add files via upload

---
+++ 52.golbal finding file .py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 18:39:34 2020
+
+@author: utob
+"""
+
+from pathlib import Path
+
+path = Path()
+for file in path.glob('**'):
+    print (file)
+
+
+# * yany hamechi
+# begard dombal hamechi
