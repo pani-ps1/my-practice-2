45f1f72939d4206ed178aadf0c54fc80fa85d471
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:18:19 -0300

    Add files via upload

---
+++ 44.constractors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 15:41:36 2020
+
+@author: utob
+"""
+
+class Point:
+    def move(self):
+        print("move")
+        
+    def draw(self):
+        print("draw")
+        
