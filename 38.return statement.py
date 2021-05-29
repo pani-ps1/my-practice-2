2396c733ec1c5f42769521b973d114c0587eea08
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:15:29 -0300

    Add files via upload

---
+++ 38.return statement.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 17:24:01 2020
+
+@author: utob
+"""
+
+# hame function ha return value shun age 
+# return nadashte bashan none hast
+
+
+
+# none = null
