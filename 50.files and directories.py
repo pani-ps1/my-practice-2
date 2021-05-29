e554c3e850068fd66b7b2ae3e7c83bfc9e5d2a52
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:20:51 -0300

    Add files via upload

---
+++ 50.files and directories.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 18:21:52 2020
+
+@author: utob
+"""
+
+from pathlib import Path
+
+
+#Absolute path
+#c:\program Files\Microsoft
+#in windows
+#Relative path
+
+path = Path("ecommerce")
+print(path.exists())
